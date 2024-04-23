8a5d0f0c9af465706a077cdd3a557f92542c054a
marianachoratto <marianachoratto@gmail.com>
2024-04-22 23:32:31 -0300

    git push de reserva

--- cypress/e2e/filmes/testesDeReviews.cy.js
+++ cypress/e2e/filmes/testesDeReviews.cy.js
@@ -1,12 +1,13 @@
 const { fakerPT_BR } = require("@faker-js/faker");
 
-describe("Teste de reviews sem permissão de administrador", () => {
+describe("Teste de reviews de usuário", () => {
   // Usuário, independente do tipo, pode criar uma review de um filme que esteja cadastrado.
 
   let userToken;
   let userId;
   let movieId;
   let tituloFilme;
+  let idDaReview;
 
   // É necessário criar o filme para saber qual filme pegar
   before(() => {
@@ -47,7 +48,6 @@ describe("Teste de reviews sem permissão de administrador", () => {
   it("usuario simples deve conseguir fazer review de filme", () => {
     cy.cadastroLogin().then((resposta) => {
       userToken = resposta.token;
-      cy.log(movieId);
 
       cy.request({
         method: "POST",
@@ -61,8 +61,106 @@ describe("Teste de reviews sem permissão de administrador", () => {
           reviewText: "Gostei muito do filme",
         },
       }).then((resposta) => {
-        cy.log(movieId);
+        idDaReview = resposta.body.id;
         expect(resposta.status).to.equal(201);
+        cy.log(idDaReview);
+      });
+    });
+  });
+
+  it("deve retornar sucesso quando o score tiver números quebrados", () => {
+    cy.cadastroLogin().then((resposta) => {
+      userToken = resposta.token;
+
+      cy.request({
+        method: "POST",
+        url: "/api/users/review",
+        auth: {
+          bearer: userToken,
+        },
+        body: {
+          movieId: movieId,
+          score: 4.73,
+          reviewText: "Gostei muito do filme",
+        },
+      }).then((resposta) => {
+        expect(resposta.status).to.equal(201);
+      });
+    });
+  });
+
+  it("deve retornar bad request quando o score foi maior que 5", () => {
+    cy.cadastroLogin().then((resposta) => {
+      userToken = resposta.token;
+
+      cy.request({
+        method: "POST",
+        url: "/api/users/review",
+        auth: {
+          bearer: userToken,
+        },
+        body: {
+          movieId: movieId,
+          score: 10,
+          reviewText: "Gostei muito do filme",
+        },
+        failOnStatusCode: false,
+      }).then((resposta) => {
+        expect(resposta.status).to.equal(400);
+        expect(resposta.body.error).to.equal("Bad Request");
+        expect(resposta.body.message).to.equal(
+          "Score should be between 1 and 5"
+        );
+      });
+    });
+  });
+
+  it("deve retornar bad request quando o score for menor que 1", () => {
+    cy.cadastroLogin().then((resposta) => {
+      userToken = resposta.token;
+
+      cy.request({
+        method: "POST",
+        url: "/api/users/review",
+        auth: {
+          bearer: userToken,
+        },
+        body: {
+          movieId: movieId,
+          score: -5,
+          reviewText: "Gostei muito do filme",
+        },
+        failOnStatusCode: false,
+      }).then((resposta) => {
+        expect(resposta.status).to.equal(400);
+        expect(resposta.body.error).to.equal("Bad Request");
+        expect(resposta.body.message).to.equal(
+          "Score should be between 1 and 5"
+        );
+      });
+    });
+  });
+
+  it("deve retornar Movie Not Found quando o filme não existir", () => {
+    cy.cadastroLogin().then((resposta) => {
+      userToken = resposta.token;
+
+      cy.request({
+        method: "POST",
+        url: "/api/users/review",
+        auth: {
+          bearer: userToken,
+        },
+        body: {
+          movieId: -9999999,
+          score: 5,
+          reviewText: "Gostei muito do filme",
+        },
+        failOnStatusCode: false,
+      }).then((resposta) => {
+        expect(resposta.status).to.equal(404);
+        expect(resposta.body.error).to.equal("Not Found");
+        expect(resposta.body.message).to.equal("Movie not found");
       });
     });
   });
@@ -74,10 +172,9 @@ describe("Teste de reviews sem permissão de administrador", () => {
       auth: {
         bearer: userToken,
       },
-    });
+    }).then((resposta) => {});
   });
 
-  // Levar para baixo
   it("Lista todas as reviews feitas pelo usuario", () => {
     cy.request({
       method: "GET",
@@ -86,53 +183,10 @@ describe("Teste de reviews sem permissão de administrador", () => {
         bearer: userToken,
       },
     }).then((resposta) => {
-      cy.log(movieId);
       expect(resposta.status).to.equal(200);
-      // expect(resposta.body[0].id).to.equal(userId);
-    });
-  });
-});
-
-describe("Teste de filmes na lista de usuários com permissão de administrador", () => {
-  let userToken;
-  let arrayNumber;
-  let userId;
-  let fixtureDoFilme;
-  let movieId;
-  let tituloFilme;
-
-  before(() => {
-    cy.cadastroLogin().then((resposta) => {
-      userToken = resposta.token;
-      userId = resposta.id;
-      cy.promoverParaAdmin(userToken).then((resposta) => {
-        cy.criarFilme(userToken).then((resposta) => {
-          movieId = resposta.id;
-          tituloFilme = resposta.title;
-
-          cy.log(movieId);
-        });
-      });
-    });
-  });
-
-  after(() => {
-    cy.promoverParaAdmin(userToken).then(() => {
-      cy.request({
-        method: "DELETE",
-        url: `/api/movies/${movieId}`,
-        headers: {
-          Authorization: "Bearer " + userToken,
-        },
-      });
-
-      cy.request({
-        method: "DELETE",
-        url: `/api/users/${userId}`,
-        auth: {
-          bearer: userToken,
-        },
-      });
+      // não está retornando nenhuma review
+      // expect(resposta.body).to.have.length(1);
+      // expect(resposta.body[0].title).to.equal(movieId);
     });
   });
 });
